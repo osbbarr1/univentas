8d961804d086d9335088fc234bcbb46fb947978c
Oscar Baquero <osbbarr1@gmail.com>
2024-12-14 10:33:10 -0500

    se adiciona actividad tres futbol colombiano 2024

---
+++ Semana2/taller/test_2.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from warnings import filterwarnings
+filterwarnings("ignore")
+import unittest
+
+class eis_test(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+
+	def test_load(self):
+		urlGeneral = 'https://www.flashscore.co/futbol/colombia/'
+		urls = ['primera-a/', 'primera-b', 'liga-femenina']
+
+		for url in urls:
+			print("Resultado -> %s" % url)
+			print("%s%s" % (urlGeneral, url))
+			self.browser.get("%s%s" % (urlGeneral, url))
+			numberList = self.browser.find_elements(By.CLASS_NAME, 'heading__info')
+			res = []
+			for number in numberList:
+				res.append(number.text)
+			self.assertEqual("2024", "2024")
+
+	def tearDown(self):
+		self.browser.quit()
+
+if __name__ == '__main__':
+	unittest.main()
